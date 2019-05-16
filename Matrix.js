9eaed921b558c140fe4de610bfea7a01ce00bf31
Corrales <ecorrales@acsicorp.com>
2019-05-15 23:37:04 -0400

    .

--- Matrix.js
+++ Matrix.js
@@ -2,6 +2,7 @@ function Matrix(rows,cols) {
 
     this.rows = rows;
     this.cols = cols;
+    this.cellFactor = 0;
     this.matrix = [];
     for (var r=0;r<this.rows;r++) {
         this.matrix[r] = [];
@@ -9,6 +10,7 @@ function Matrix(rows,cols) {
             this.matrix[r][c] = 0;
         }
     };
+
 }
 
 Matrix.prototype.add = function(n) {
@@ -30,7 +32,58 @@ Matrix.prototype.mult = function(n) {
 Matrix.prototype.randomize = function(n) {
     for (var r=0;r<this.rows;r++) {
         for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] = random(-1,1);
+            this.matrix[r][c] = random(-1,1).toFixed(2);
+        }
+    };
+}
+
+Matrix.prototype.incVal = function() {
+    let value = parseFloat(this.cellFactor);
+    //if (value < 0.1) value += parseFloat(0.01);
+    //else if (value < 1) value += parseFloat(0.1);
+    if (value < 0.1) value += 0.01;
+    else if (value < 1) value += 0.1;
+    else if (value < 10) value += 0.5;
+    else if (value < 50) value += 1;
+    else if (value < 100) value += 5;
+    else if (value < 500) value += 10;
+    else if (value < 1000) value += 50;
+    else value += 100;
+    //this.cellFactor = parseFloat(value).toFixed(2);
+    this.cellFactor = value.toFixed(2);
+};
+
+Matrix.prototype.decVal = function() {
+    let value = parseFloat(this.cellFactor);
+    if (Math.abs(value) < 0.1) value -= 0.01;
+    else if (Math.abs(value) < 1) value -= 0.1;
+    else if (Math.abs(value) < 10) value -= 0.5;
+    else if (Math.abs(value) < 50) value -= 1;
+    else if (Math.abs(value) < 100) value -= 5;
+    else if (Math.abs(value) < 500) value -= 10;
+    else if (Math.abs(value) < 1000) value -= 50;
+    else value -= 100;
+    //this.cellFactor = parseFloat(value).toFixed(2);
+    this.cellFactor = value.toFixed(2);
+};
+
+
+Matrix.prototype.addVal = function(n) {
+    for (var r=0;r<this.rows;r++) {
+        for (var c=0;c<this.cols;c++) {
+            this.matrix[r][c] += parseFloat(this.cellFactor);
+            var val = parseFloat(this.matrix[r][c]);
+            this.matrix[r][c] = parseFloat(val.toFixed(2));
+        }
+    };
+}
+
+Matrix.prototype.multVal = function(n) {
+    for (var r=0;r<this.rows;r++) {
+        for (var c=0;c<this.cols;c++) {
+            this.matrix[r][c] *= parseFloat(this.cellFactor);
+            var val = parseFloat(this.matrix[r][c]);
+            this.matrix[r][c] = parseFloat(val.toFixed(2));
         }
     };
 }
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const canvasWidth = 800;
+const canvasHeight = 500;
 const numInputNeurons = 3; //num bdrooms, num bathrooms, square footage
 const numHiddenNeurons = 4;
 const numOutputNeurons = 2;
@@ -6,14 +8,20 @@ const numBaths = 2;
 const squareFeet = 1000;
 const canvasElem = document.getElementById('myCanvas');
 const matrixElem = document.getElementById('myMatrix');
+const factorElem = document.getElementById('cellFactor');
 
-var matrix = new Matrix(numMatrixRows,numMatrixCols);
 var numMatrixRows = 3;
 var numMatrixCols = 2;
+var matrix = new Matrix(numMatrixRows,numMatrixCols);
+var redoMatrixTable = false;
 
+canvasElem.style.width = canvasWidth;
+canvasElem.style.height = canvasHeight;
 
 const createNewMatrix = () => {
     matrix = new Matrix(numMatrixRows,numMatrixCols);
+    redoMatrixTable = true;
+    factorElem.innerHTML = matrix.cellFactor;
 }
 
 const doIncRows = () => {
@@ -22,7 +30,7 @@ const doIncRows = () => {
 }
 
 const doDecRows = () => {
-    if (numMatrixRows>1) numMatrixRows--;
+    if (numMatrixRows>1) { numMatrixRows--; }
     createNewMatrix();
 }
 
@@ -32,12 +40,33 @@ const doIncCols = () => {
 }
 
 const doDecCols = () => {
-    if (numMatrixCols>1) numMatrixCols--;
+    if (numMatrixCols>1) { numMatrixCols--; }
     createNewMatrix();
 }
 
 const doRandomize = () => {
     matrix.randomize();
+    redoMatrixTable = true;
+}
+
+const doIncVal = () => {
+    matrix.incVal();
+    factorElem.innerHTML = matrix.cellFactor;
+}
+
+const doDecVal = () => {
+    matrix.decVal();
+    factorElem.innerHTML = matrix.cellFactor;
+}
+
+const doAddVal = () => {
+    matrix.addVal();
+    redoMatrixTable = true;
+}
+
+const doMultVal = () => {
+    matrix.multVal();
+    redoMatrixTable = true;
 }
 
 function displayMatrix() {
@@ -45,18 +74,18 @@ function displayMatrix() {
     for (var r=0;r<matrix.rows;r++) {
         html += '<tr>';
         for (var c=0;c<matrix.cols;c++) {
-            html += '<td>' + matrix.matrix[r][c] + '</td>';
+            html += '<td id="row'+r+'col'+c+'" ><input type="text" value="'+ matrix.matrix[r][c] + '" maxlength="7" size="7"/></td>';
         }
         html += '</tr>';
     };
     matrixElem.innerHTML = html;
+    redoMatrixTable = false;
 }
 
 var brain = new NeuralNetwork(numInputNeurons, numHiddenNeurons, numOutputNeurons);
 
 function setup() {
-    var canvas = createCanvas(700, 400);
-    canvasElem.style.width = ''
+    var canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('myCanvas');
     background(0);
     displayMatrix();
@@ -65,7 +94,6 @@ function setup() {
 function draw() {
     background(0);
     ellipse(width/2,height/2,width,height);
-    displayMatrix();
+    if (redoMatrixTable) displayMatrix();
 }
 
-displayMatrix();
