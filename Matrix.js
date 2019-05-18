12bba8dcb6bf5de8fca502863bf3c1fcb6feb947
Corrales <ecorrales@acsicorp.com>
2019-05-18 11:43:40 -0400

    .

--- Matrix.js
+++ Matrix.js
@@ -1,89 +1,112 @@
-function Matrix(rows,cols) {
+class Matrix {
 
-    this.rows = rows;
-    this.cols = cols;
-    this.cellFactor = 0;
-    this.matrix = [];
-    for (var r=0;r<this.rows;r++) {
-        this.matrix[r] = [];
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] = 0;
+    constructor(rows,cols,name) {
+        this.name = name;
+        this.rows = rows;
+        this.cols = cols;
+        this.cellFactor = 0;
+        this.matrix = [];
+        this.dotMatrix;
+        for (let r=0;r<this.rows;r++) {
+            this.matrix[r] = [];
+            for (let c=0;c<this.cols;c++) {
+                this.matrix[r][c] = 0;
+            }
         }
-    };
+    }
 
-}
+    add(n) {
+        if (n instanceof Matrix && n.rows === this.rows && n.cols === this.cols) { 
+            for (let r=0;r<this.rows;r++) {
+                for (let c=0;c<this.cols;c++) {
+                    this.matrix[r][c] += parseFloat(n.matrix[r][c]);
+                }
+            }
 
-Matrix.prototype.add = function(n) {
-    for (var r=0;r<this.rows;r++) {
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] += n;
+        } else if (n instanceof Matrix) { 
+            return false;
+        } else if (n !== undefined) {
+            for (let r=0;r<this.rows;r++) {
+                for (let c=0;c<this.cols;c++) {
+                    let v = parseFloat(n);
+                    this.matrix[r][c] += parseFloat(v);
+                }
+            }
+        } else {
+            return false;
         }
-    };
-}
+        return true;
+    }
 
-Matrix.prototype.mult = function(n) {
-    for (var r=0;r<this.rows;r++) {
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] *= n;
+    mult(n, type) {
+        if (n instanceof Matrix && type === 'hadamard' && n.rows === this.rows && n.cols === this.cols) { 
+            for (let r=0;r<this.rows;r++) {
+                for (let c=0;c<this.cols;c++) {
+                    this.matrix[r][c] *= parseFloat(n.matrix[r][c]);
+                }
+            }
+
+        } else if (n instanceof Matrix && type === 'dot' && this.cols === n.rows) { 
+            this.dotMatrix = new Matrix(n.rows,this.cols,'dot'+this.name);
+
+        } else if (n instanceof Matrix) { 
+            return false;
+        } else if (n !== undefined) {
+            for (let r=0;r<this.rows;r++) {
+                for (let c=0;c<this.cols;c++) {
+                    let v = parseFloat(n);
+                    this.matrix[r][c] *= parseFloat(v);
+                }
+            }
+        } else {
+            return false;
         }
-    };
-}
+        return true;
+    }
 
-Matrix.prototype.randomize = function(n) {
-    for (var r=0;r<this.rows;r++) {
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] = random(-1,1).toFixed(2);
+    randomize() {
+        for (let r=0;r<this.rows;r++) {
+            for (let c=0;c<this.cols;c++) {
+                this.matrix[r][c] = random(-1,1);
+            }
         }
-    };
-}
+    }
 
-Matrix.prototype.incVal = function() {
-    let value = parseFloat(this.cellFactor);
-    //if (value < 0.1) value += parseFloat(0.01);
-    //else if (value < 1) value += parseFloat(0.1);
-    if (value < 0.1) value += 0.01;
-    else if (value < 1) value += 0.1;
-    else if (value < 10) value += 0.5;
-    else if (value < 50) value += 1;
-    else if (value < 100) value += 5;
-    else if (value < 500) value += 10;
-    else if (value < 1000) value += 50;
-    else value += 100;
-    //this.cellFactor = parseFloat(value).toFixed(2);
-    this.cellFactor = value.toFixed(2);
-};
+    incVal() {
+        let value = parseFloat(this.cellFactor);
+        //if (value < 0.1) value += parseFloat(0.01);
+        //else if (value < 1) value += parseFloat(0.1);
+        if (Math.abs(value) < 0.1) value += 0.01;
+        else if (Math.abs(value) < 1) value += 0.1;
+        else if (Math.abs(value) < 10) value += 0.5;
+        else if (Math.abs(value) < 50) value += 1;
+        else if (Math.abs(value) < 100) value += 5;
+        else if (Math.abs(value) < 500) value += 10;
+        else if (Math.abs(value) < 1000) value += 50;
+        else value += 100;
+        this.cellFactor = value.toFixed(2);
+    }
 
-Matrix.prototype.decVal = function() {
-    let value = parseFloat(this.cellFactor);
-    if (Math.abs(value) < 0.1) value -= 0.01;
-    else if (Math.abs(value) < 1) value -= 0.1;
-    else if (Math.abs(value) < 10) value -= 0.5;
-    else if (Math.abs(value) < 50) value -= 1;
-    else if (Math.abs(value) < 100) value -= 5;
-    else if (Math.abs(value) < 500) value -= 10;
-    else if (Math.abs(value) < 1000) value -= 50;
-    else value -= 100;
-    //this.cellFactor = parseFloat(value).toFixed(2);
-    this.cellFactor = value.toFixed(2);
-};
+    decVal() {
+        let value = parseFloat(this.cellFactor);
+        if (Math.abs(value) < 0.1) value -= 0.01;
+        else if (Math.abs(value) < 1) value -= 0.1;
+        else if (Math.abs(value) < 10) value -= 0.5;
+        else if (Math.abs(value) < 50) value -= 1;
+        else if (Math.abs(value) < 100) value -= 5;
+        else if (Math.abs(value) < 500) value -= 10;
+        else value -= 100;
+        this.cellFactor = value.toFixed(2);
+    }
 
 
-Matrix.prototype.addVal = function(n) {
-    for (var r=0;r<this.rows;r++) {
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] += parseFloat(this.cellFactor);
-            var val = parseFloat(this.matrix[r][c]);
-            this.matrix[r][c] = parseFloat(val.toFixed(2));
-        }
-    };
-}
+    addVal() {
+        this.add(this.cellFactor);
+    }
 
-Matrix.prototype.multVal = function(n) {
-    for (var r=0;r<this.rows;r++) {
-        for (var c=0;c<this.cols;c++) {
-            this.matrix[r][c] *= parseFloat(this.cellFactor);
-            var val = parseFloat(this.matrix[r][c]);
-            this.matrix[r][c] = parseFloat(val.toFixed(2));
-        }
-    };
+    multVal() {
+        return this.mult(this.cellFactor);
+    }
+
+
 }
--- index.js
+++ index.js
@@ -6,94 +6,288 @@ const numOutputNeurons = 2;
 const numBedrooms = 3;
 const numBaths = 2;
 const squareFeet = 1000;
+const messagesElem = document.getElementById('messages');
 const canvasElem = document.getElementById('myCanvas');
-const matrixElem = document.getElementById('myMatrix');
-const factorElem = document.getElementById('cellFactor');
+const matrix1Elem = document.getElementById('myMatrix');
+const dotMatrix1Elem = document.getElementById('myDotMatrix');
+const matrix2Elem = document.getElementById('myMatrix2');
+const dotMatrix2Elem = document.getElementById('myDotMatrix2');
+const factor1Elem = document.getElementById('cellFactor1');
+const factor2Elem = document.getElementById('cellFactor2');
+const matrix1SelectElem = document.getElementById('selectMatrix1');
+const matrix2SelectElem = document.getElementById('selectMatrix2');
 
-var numMatrixRows = 3;
-var numMatrixCols = 2;
-var matrix = new Matrix(numMatrixRows,numMatrixCols);
-var redoMatrixTable = false;
+
+let numMatrixRows = 3;
+let numMatrixCols = 2;
+let matrix1 = new Matrix(numMatrixRows,numMatrixCols,'Matrix1');
+let matrix2 = new Matrix(numMatrixRows,numMatrixCols,'Matrix2');
+let redoMatrixTable = false;
 
 canvasElem.style.width = canvasWidth;
 canvasElem.style.height = canvasHeight;
 
+matrix1Selected = false;
+matrix2Selected = false;
+
+const showMessage = (type, message) => {
+    messagesElem.innerHTML = message;
+    messagesElem.className = 'alert alert-' + type;
+}
+
+const clearMessage = () => {
+    messagesElem.innerHTML = '';
+    messagesElem.className = '';
+}
+
 const createNewMatrix = () => {
-    matrix = new Matrix(numMatrixRows,numMatrixCols);
+    if (matrix1Selected) {
+        matrix1 = new Matrix(numMatrixRows,numMatrixCols,'Matrix1');
+    }
+    if (matrix2Selected) {
+        matrix2 = new Matrix(numMatrixRows,numMatrixCols,'Matrix2');
+    }
     redoMatrixTable = true;
-    factorElem.innerHTML = matrix.cellFactor;
 }
 
 const doIncRows = () => {
+    clearMessage();
     numMatrixRows++;
-    createNewMatrix();
+    if (matrix1Selected) {
+        createNewMatrix();
+    }
+    if (matrix2Selected) {
+        createNewMatrix();
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doDecRows = () => {
+    clearMessage();
     if (numMatrixRows>1) { numMatrixRows--; }
-    createNewMatrix();
+    if (matrix1Selected) {
+        createNewMatrix();
+    }
+    if (matrix2Selected) {
+        createNewMatrix();
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doIncCols = () => {
+    clearMessage();
     numMatrixCols++;
-    createNewMatrix();
+    if (matrix1Selected) {
+        createNewMatrix();
+    }
+    if (matrix2Selected) {
+        createNewMatrix();
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doDecCols = () => {
+    clearMessage();
     if (numMatrixCols>1) { numMatrixCols--; }
-    createNewMatrix();
-}
-
-const doRandomize = () => {
-    matrix.randomize();
-    redoMatrixTable = true;
+    if (matrix1Selected) {
+        createNewMatrix();
+    }
+    if (matrix2Selected) {
+        createNewMatrix();
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doIncVal = () => {
-    matrix.incVal();
-    factorElem.innerHTML = matrix.cellFactor;
+    clearMessage();
+    if (matrix1Selected) {
+        matrix1.incVal();
+        factor1Elem.innerHTML = parseFloat(matrix1.cellFactor).toFixed(2);
+    }
+    if (matrix2Selected) {
+        matrix2.incVal();
+        factor2Elem.innerHTML = parseFloat(matrix2.cellFactor).toFixed(2);
+    } 
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doDecVal = () => {
-    matrix.decVal();
-    factorElem.innerHTML = matrix.cellFactor;
+    clearMessage();
+    if (matrix1Selected) {
+        matrix1.decVal();
+        factor1Elem.innerHTML = parseFloat(matrix1.cellFactor).toFixed(2);
+    }
+    if (matrix2Selected) {
+        matrix2.decVal();
+        factor2Elem.innerHTML = parseFloat(matrix2.cellFactor).toFixed(2);
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
 }
 
 const doAddVal = () => {
-    matrix.addVal();
+    clearMessage();
+    if (matrix1Selected) {
+        matrix1.addVal();
+    }
+    if (matrix2Selected) {
+        matrix2.addVal();
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
     redoMatrixTable = true;
 }
 
 const doMultVal = () => {
-    matrix.multVal();
+    clearMessage();
+    if (matrix1Selected) {
+        if (!matrix1.multVal()) {
+            showMessage('danger','Matrix Sizes Dont Align'); 
+        }
+    }
+    if (matrix2Selected) {
+        if (!matrix2.multVal()) {
+            showMessage('danger','Matrix Sizes Dont Align'); 
+        }
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
     redoMatrixTable = true;
 }
 
-function displayMatrix() {
+const doRandomize = () => {
+    clearMessage();
+    if (matrix1Selected) {
+        matrix1.randomize();
+        factor1Elem.innerHTML = matrix.cellFactor;
+    }
+    if (matrix2Selected) {
+        matrix2.randomize();
+        factor2Elem.innerHTML = matrix2.cellFactor;
+    }
+    if (!matrix1Selected && !matrix2Selected) {
+        showMessage('danger','No Matrix Selected');
+    }
+    redoMatrixTable = true;
+}
+
+const doSelectMatrix1 = () => {
+    matrix1Selected = matrix1Selected?false:true;
+    if (matrix1Selected) {
+        matrix1SelectElem.className = 'btn btn-danger btn-block';
+        matrix1SelectElem.innerHTML = 'Selected';
+    } else {
+        matrix1SelectElem.className = 'btn btn-secondary btn-block';
+        matrix1SelectElem.innerHTML = 'Select';
+    }
+    clearMessage();
+}
+
+const doSelectMatrix2 = () => {
+    matrix2Selected = matrix2Selected?false:true;
+    if (matrix2Selected) {
+        matrix2SelectElem.className = 'btn btn-danger btn-block';
+        matrix2SelectElem.innerHTML = 'Selected';
+    } else {
+        matrix2SelectElem.className = 'btn btn-secondary btn-block';
+        matrix2SelectElem.innerHTML = 'Select';
+    }
+    clearMessage();
+}
+
+const doAddByMatrix2 = () => {
+    clearMessage();
+    if (!matrix1.add(matrix2)) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+const doHadamardByMatrix2 = () => {
+    clearMessage();
+    if (!matrix1.mult(matrix2,'hadamard')) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+const doDotProdByMatrix2 = () => {
+    clearMessage();
+    if (!matrix1.mult(matrix2,'dot')) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+const doAddByMatrix1 = () => {
+    clearMessage();
+    if (!matrix2.add(matrix1)) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+const doHadamardByMatrix1 = () => {
+    clearMessage();
+    if (!matrix2.mult(matrix1,'hadamard')) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+const doDotProdByMatrix1 = () => {
+    clearMessage();
+    if (!matrix2.mult(matrix1,'dot')) {
+        showMessage('danger','Matrix Sizes Dont Align'); 
+    }
+    redoMatrixTable = true;
+}
+
+
+function displayMatrix(whichMatrixElem, whichMatrix) {
     let html = '';
-    for (var r=0;r<matrix.rows;r++) {
+    for (let r=0;r<whichMatrix.rows;r++) {
         html += '<tr>';
-        for (var c=0;c<matrix.cols;c++) {
-            html += '<td id="row'+r+'col'+c+'" ><input type="text" value="'+ matrix.matrix[r][c] + '" maxlength="7" size="7"/></td>';
+        for (let c=0;c<whichMatrix.cols;c++) {
+            html += '<td id="row'+r+'col'+c+'" ><input type="text" value="'+ parseFloat(whichMatrix.matrix[r][c]).toFixed(2) + '" maxlength="7" size="7"/></td>';
         }
         html += '</tr>';
     };
-    matrixElem.innerHTML = html;
+    whichMatrixElem.innerHTML = html;
     redoMatrixTable = false;
 }
 
-var brain = new NeuralNetwork(numInputNeurons, numHiddenNeurons, numOutputNeurons);
+let brain = new NeuralNetwork(numInputNeurons, numHiddenNeurons, numOutputNeurons);
 
 function setup() {
-    var canvas = createCanvas(canvasWidth, canvasHeight);
+    let canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('myCanvas');
     background(0);
-    displayMatrix();
+    displayMatrix(matrix1Elem, matrix1);
+    displayMatrix(matrix2Elem, matrix2);
 }
 
 function draw() {
     background(0);
     ellipse(width/2,height/2,width,height);
-    if (redoMatrixTable) displayMatrix();
+    if (redoMatrixTable) { 
+        displayMatrix(matrix1Elem, matrix1);
+        if (matrix1.dotMatrix !== undefined) { displayMatrix(dotMatrix1Elem, matrix1.dotMatrix); }
+        displayMatrix(matrix2Elem, matrix2);
+        if (matrix2.dotMatrix !== undefined) { displayMatrix(dotMatrix2Elem, matrix2.dotMatrix); }
+    }
 }
 
